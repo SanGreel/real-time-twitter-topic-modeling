3b428be7c43e25cf48a6ae6d48e83fe5953f2700
SanGreel <sangreel1@gmail.com>
2019-07-18 22:12:29 +0000

    pytrends updated

--- utils/trends.py
+++ utils/trends.py
@@ -6,6 +6,16 @@ from pyspark.sql import Row
 wrong_date = datetime.strptime("Mon Jun 03 00:00:00 +0000 2000", '%a %b %d %H:%M:%S %z %Y')
 
 def validate(date_text):
+    """
+    Validate date in str
+
+    Parameters:
+    date_text (str):  datetimne in str format 
+
+    Returns:
+    (boolean): boolean status about validation 
+    """
+    
     try:
         if date_text != datetime.strptime(date_text, '%a %b %d %H:%M:%S %z %Y').strftime('%a %b %d %H:%M:%S %z %Y'):
             raise ValueError
@@ -13,82 +23,53 @@ def validate(date_text):
     except ValueError:
         return False
 
+    
 def str_tweet_to_datetime(frame_datetime):
+    
+    """
+    Convert tweet date to datetime
+
+    Parameters:
+    frame_datetime (str):  tweet frame date in str 
+
+    Returns:
+    datetime (datetimne): converted datetime format 
+    """
+
+
     if (validate(frame_datetime) == True):
         return datetime.strptime(frame_datetime,'%a %b %d %H:%M:%S %z %Y')
     else:
         return wrong_date
 
 def datetime_to_tweet_str(frame_datetime):
+    
+    """
+    Convert datetime to str
+
+    Parameters:
+    frame_datetime (str):  datetime 
+
+    Returns:
+    ts (str): datetime in str format
+    """
+
     ts = datetime.strftime(frame_datetime, '%a %b %d %H:%M:%S %z %Y')
     return ts
 
-def tweet2google_timeframe(frame_start_datetime, frame_finish_datetime):
-    start_date = str_tweet_to_datetime(frame_start_datetime)
-    end_date = str_tweet_to_datetime(frame_finish_datetime)
-    tim
     
-def get_google_trends_by_geo(geo, google_trends_dict):
-    if geo in google_trends_dict:
-        return google_trends_dict[geo][0], google_trends_dict[geo][1]
-    
-    return None, None
-    
-def unique_google_trends_by_time_frame(df, spark):
-    data = df.collect()
-    rising_dict = {}
-    top_dict = {}
-    
-    geo = data[0]['geo']
-    columns = df.columns
-
-    for i in range(0, len(data)):
-        rising_val = data[i][columns[1]]
-        top_value = data[i][columns[2]]
-        
-        if rising_val in rising_dict:
-            rising_dict[rising_val][0] += str_rising_to_float(data[i][columns[3]])
-            rising_dict[rising_val][1] += 1
-        else:
-            rising_dict[rising_val] = [str_rising_to_float(data[i][columns[3]]), 1]
-            
-        if top_value in top_dict:
-            top_dict[top_value][0] += float(data[i][columns[4]])
-            top_dict[top_value][1] += 1
-        else:
-            top_dict[top_value] = [float(data[i][columns[4]]), 1]
-    
-    
-    for key in top_dict:
-        top_dict[key] = round(top_dict[key][0] / top_dict[key][1])
-        
-    for key in rising_dict:
-        rising_dict[key] = round(rising_dict[key][0] / rising_dict[key][1])
-    
-    top_dict = sorted(top_dict.items(), key=operator.itemgetter(1), reverse=True)
-    rising_dict = sorted(rising_dict.items(), key=operator.itemgetter(1), reverse=True)
-    
-    
-    seq = []
-    len_top = len(top_dict)
-    len_rising = len(rising_dict)
-    length = max(len_top, len_rising)
-    
-    row = Row(columns[1], columns[2], columns[3], columns[4], columns[5])
-    
-    for i in range(0, length):
-        rising = rising_dict[i][0] if i < len_rising else ''
-        rising_val = f"+{rising_dict[i][1]}%" if i < len_rising else None
-        
-        top = top_dict[i][0] if i < len_top else ''
-        top_val = top_dict[i][1] if i < len_top else None
-        
-        seq.append(row(rising, top, rising_val, top_val, geo))
-    
-    dframe = spark.createDataFrame(seq)
-    return dframe
-
 def get_geo_name(geo):
+    
+    """
+    get geo information
+
+    Parameters:
+    geo (str):  geo US or US-NY 
+
+    Returns:
+    (str): get extended geo information
+    """
+                
     if geo == "US-NY":
         return "New York"
     elif geo == "US":
@@ -96,39 +77,23 @@ def get_geo_name(geo):
     return ""
 
 def print_google_trend_title(start_date, finish_date, name, geo):
+    
+    """
+    print google trends in suitable way
+
+    Parameters:
+    start_date (datetime):  start date 
+    
+    finish_date (datetime): end date
+    
+    geo (str) geo info
+    
+    """
     start_date_str = start_date.strftime("%Y-%m-%d")
+    
     if start_date == finish_date:
         print(f"\nGoogle trends {name} in {get_geo_name(geo)} during {start_date_str}")
     else:
         finish_date_str = finish_date.strftime("%Y-%m-%d")
         print(f"\nGoogle trends {name} in {get_geo_name(geo)} during {start_date_str} - {finish_date_str}")
         
-def convert_datetime_in_interesting_google(df):
-    columns = df.columns
-    converted_df = df.rdd.map(lambda x : (
-                                          x["Date"].strftime("%Y-%m-%d"), 
-                                          x[columns[1]], 
-                                          x[columns[2]], 
-                                          x[columns[3]],
-                                          x[columns[4]],
-                                          x[columns[5]])).toDF([columns[0], columns[1], columns[2], columns[3], columns[4], columns[5]])
-                                                
-    return converted_df
-
-def str_rising_to_float(str):
-    if str is None:
-        return 0.0
-    if str == '':
-        return 0.0
-    if str == 'Breakout':
-        return 0.0
-    
-    str_value = str.split('%')[0]
-    if '+' in str_value:
-        str_value = str_value.split('+')[1]
-        
-    if ',' in str_value:
-        str_value = str_value.replace(',', '.')
-        value = 1000* float(str_value)
-        return value
-    return float(str_value)
