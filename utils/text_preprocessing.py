05ac3229d8732f3071b30009bc50c0d2b56b1e30
SanGreel <sangreel1@gmail.com>
2019-07-19 14:42:45 +0000

    url support and outline were added

--- utils/text_preprocessing.py
+++ utils/text_preprocessing.py
@@ -1,5 +1,6 @@
 import re
 import nltk
+import urllib
 from nltk.stem import WordNetLemmatizer 
 from utils.channels_to_filter import channels_not_to_consider
 
@@ -83,6 +84,85 @@ def process_hashtags(tweet, lemmatizer=lemmatizer, stop_word_list=stop_word_list
     return hashtags
 
 
+def get_response(url):
+    
+    """Get response by url.
+
+    Parameters:
+    url (str): url to query
+
+    Returns:
+    str: string which represents html response from url or None if url doesn't respond
+
+   """
+    
+    try:
+        response = urllib.request.urlopen(url).read().decode()
+        return response
+    except:
+        return None
+    
+
+def get_keywords(html):
+    
+    """Parse keywords from html
+
+    Parameters:
+    html (str): html to parse
+
+    Returns:
+    str: keywords
+
+   """
+    
+    # use regex to find meta
+    keywords = re.findall('<meta name="keywords" content="(.+)"', html)
+    
+    if len(keywords) > 0:
+        keywords = keywords[0]
+    
+        # filter keywrods
+        keywords = keywords.lower() # get lowercase
+        keywords = re.sub(r'[^A-Za-z]', ' ', keywords) # filter all non-letters
+        keywords = re.sub(r'\s{2,}', ' ', keywords) # remove multiply whitespaces
+        keywords = re.sub(r'(.)\1{2,}', r'\1', keywords) # remove repeated chars (e.g. "greeeeat" -> "great")
+        keywords = keywords.strip() # remove possible whitespaces from both sides of the tweet
+    else:
+        keywords = ''
+    
+    return keywords
+
+
+def get_description(html):
+    
+    """Parse description from html
+
+    Parameters:
+    html (str): html to parse
+
+    Returns:
+    str: description
+
+   """
+    
+    # use regex to find meta
+    description = re.findall('<meta name="description" content="(.+)"', html)
+    
+    if len(description) > 0:
+        description = description[0]
+    
+        # filter description
+        description = description.lower() # get lowercase
+        description = re.sub(r'[^A-Za-z]', ' ', description) # filter all non-letters
+        description = re.sub(r'\s{2,}', ' ', description) # remove multiply whitespaces
+        description = re.sub(r'(.)\1{2,}', r'\1', description) # remove repeated chars (e.g. "greeeeat" -> "great")
+        description = description.strip() # remove possible whitespaces from both sides of the tweet
+    else:
+        description = ''
+    
+    return description
+   
+    
 def process_urls(tweet, tokenizer=tokenizer):
     
     """Filter out all text except urls and parse them
@@ -94,8 +174,33 @@ def process_urls(tweet, tokenizer=tokenizer):
     stop_word_list (list): list with stop words
 
     Returns:
-    list: list of tokens, which represent input tweet 
+    list: list of tokens from keywords from url
+    list: list of tokens from url's description
 
    """
     
-    pass
+    # find urls in tweet
+    urls = re.findall(r"https(.+)", tweet)
+                                  
+    if len(urls) > 0:                              
+        urls = ['https' + url for url in urls]
+
+        # query urls
+        responses = [get_response(url) for url in urls]
+
+        # parse urls for keywords and description
+        keywords = [[_ for _ in tokenizer.tokenize(get_keywords(response))] for response in responses if response is not None]
+        descriptions = [[_ for _ in tokenizer.tokenize(get_description(response))] for response in responses if response is not None]
+    
+        # flatten lists
+        flatten = lambda l: [item for sublist in l for item in sublist]
+        keywords = flatten(keywords)
+        descriptions = flatten(descriptions)
+    
+        # merging keywords with descriptions
+        keywords.extend(descriptions)
+        
+        return keywords
+    
+    else:
+        return []
